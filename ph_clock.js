a3a2a72bce6edec49be2c58ec14e6e149668c70e
Unknown <mjohnson030@west-mec.org>
2019-02-14 18:23:57 -0700

    Started Writing Functions
    Began writing a countDown function and an addLeadingZero function, and checkTimer function.

--- ph_clock.js
+++ ph_clock.js
@@ -4,8 +4,8 @@
    Case Problem 4
 
    Countdown Clock for Ticket Ordering
-   Author: 
-   Date:  
+   Author: Millione Johnson
+   Date: 2/14/19 
 
    Filename:   ph_clock.js     
 
@@ -23,6 +23,20 @@
    ticket order. If the time left is 0, the stopClock() function is run;
    otherwise nothing happens and the program continues to run. */
    
+    
+ var minsLeft = 0;
+ var secsLeft = 15;
+ var timeLeft  = (minsLeft * 60) + secsLeft; 
+ var secsString =   
+
+ function countDown(){
+ minsLeft = Math.floor(timeLeft/60);
+ secsLeft = (timeLeft - 60) * minsLeft;
+ addLeadingZero (minsString = addLeadingZero(minsLeft));
+ addLeadingZero (secsString);
+ getElementByID(minsString = );
+}
+
 function checkTimer() {
    if (timeLeft === 0) stopClock();
 }
@@ -34,3 +48,5 @@ function addLeadingZero(num) {
    var numStr = (num < 10) ? ("0" + num) : "" + num;
    return numStr;
 }
+ 
+
